bedde5918ce84c18278a16cfb9182bda7b383176
pangonis <56597665+pangonis@users.noreply.github.com>
2019-10-15 19:52:26 +0000

    done with day 1

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,17 @@
 
 import './styles.css';
-console.log('Ready to Party With Some TypeScript!');
+import { multiply } from './math';
 
+const num1 = document.getElementById('number1') as HTMLInputElement;
+const num2 = document.getElementById('number2') as HTMLInputElement;
+const multiplybutton = document.getElementById('multiply') as HTMLInputElement;
+const answerEl = document.getElementById('answer') as HTMLSpanElement;
+
+multiplybutton.addEventListener('click', function () {
+    const n1 = num1.valueAsNumber;
+    const n2 = num2.valueAsNumber;
+
+    const answer = multiply(n1, n2);
+
+    answerEl.innerHTML = answer.toString();
+});
