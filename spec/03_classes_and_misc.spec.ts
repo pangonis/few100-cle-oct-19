cad6af2aab349f13a30210624cc5f20b3b3427c9
pangonis <56597665+pangonis@users.noreply.github.com>
2019-10-17 15:38:19 +0000

    commit

--- spec/03_classes_and_misc.spec.ts
+++ spec/03_classes_and_misc.spec.ts
@@ -1,6 +1,6 @@
 //import {Employee} from "./hr/employee";
 import * as stuff from './hr';
-import {Raisable} from './hr';
+import { Raisable } from './hr';
 //import { Employee, Department} from './hr';
 
 describe('classes', () => {
@@ -23,20 +23,42 @@ describe('classes', () => {
 })
 
 describe('miscellaneous', () => {
-    const numbers = [1,2,3,4,5];
+    const numbers = [1, 2, 3, 4, 5];
     it('immutably add an element to an array', () => {
         const newNumbers = [0, ...numbers];
 
-        expect(newNumbers).toEqual([0,1,2,3,4,5]);
+        expect(newNumbers).toEqual([0, 1, 2, 3, 4, 5]);
     })
     it('immutably removing an element from an array', () => {
         const newNumbers = numbers.filter(n => n !== 3);
-        expect(newNumbers).toEqual([1,2,4,5]);
+        expect(newNumbers).toEqual([1, 2, 4, 5]);
     })
     it('changing a property of an object immediately', () => {
-        const movies = {title: "ep 4", yearReleased: 1978};
-        const newMovie = {...movies, yearReleased: 1977};
+        const movies = { title: "ep 4", yearReleased: 1978 };
+        const newMovie = { ...movies, yearReleased: 1977 };
         expect(newMovie.yearReleased).toBe(1977);
         expect(movies.yearReleased).toBe(1978);
     })
+    it('array destructuring', () => {
+
+        // const first = numbers[0];
+        // const third = numbers[2];
+
+        const [first, , third] = numbers;
+        expect(first).toBe(1);
+        expect(third).toBe(3);
+
+    });
+    it('object destructuring', () => {
+        const movie = { title: 'Episode IV: A New Hope', yearReleased: 1977 };
+
+        // const title = movie.title;
+        // const year = movie.yearReleased;
+
+        const { yearReleased: year, title } = movie;
+
+
+        expect(title).toBe('Episode IV: A New Hope');
+        expect(year).toBe(1977);
+    });
 })
